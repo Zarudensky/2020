a03a1afcd49e01166ed45f4502ffdf772528ecbc
Zarudensky <37154263+Zarudensky@users.noreply.github.com>
2019-12-30 16:53:00 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+window.onload=function(){
+  var merrywrap=document.getElementById("merrywrap");
+  var box=merrywrap.getElementsByClassName("giftbox")[0];
+  var step=1;
+  var stepMinutes=[2000,2000,1000,1000];
+  function init(){
+          box.addEventListener("click",openBox,false);
+  }
+  function stepClass(step){
+    merrywrap.className='merrywrap';
+    merrywrap.className='merrywrap step-'+step;
+  }
+  function openBox(){
+    if(step===1){
+      box.removeEventListener("click",openBox,false); 
+    }  
+    stepClass(step); 
+    if(step===3){ 
+    } 
+    if(step===4){ 
+       return;
+    }     
+    setTimeout(openBox,stepMinutes[step-1]);
+    step++;  
+  }
+   
+  init();
+}
+
+$(document).ready(function() {
+  const openBox = $('.giftbox');
+  const sound = $('#volume');
+  let audio = new Audio('./audio/box.mp3');
+      
+  openBox.click(function() {
+    $('.descr').hide();
+    audio.play();
+    audio = new Audio('./audio/magic.mp3');
+    audio.play();
+    sound.addClass('on');      
+  });
+  sound.click(function() {
+    if(sound.hasClass('on')) {
+      sound.removeClass('on');
+      audio.pause();
+      sound.addClass('off');
+    } else {
+      sound.removeClass('off');
+      audio.play();
+      sound.addClass('on');
+    }      
+  });
+});
+
+
+ 
---
+++ js/text.js
@@ -0,0 +1,136 @@
+const congratulations = {
+  "parents": { //?name=parents
+    "name": "Мама и Папа",
+    "text1": "Дорогие мои родители, люблю Вас очень!",
+    "text2": "Крепкого здоровья и хорошего настроения.",
+    "text3": "С Новым Годом!"
+  },
+  "brother": { //?name=brother
+    "name": "Влад и Марина",
+    "text1": "Родные мои путешественники)",
+    "text2": "Финансовой стабильности и новых стран.",
+    "text3": "С Новым Годом!"
+  },
+  "nephews": { //?name=nephews
+    "name": "Тема и Даня",
+    "text1": "Любимые мои племянники!",
+    "text2": "Успехов во всех начинаниях.",
+    "text3": "С Новым Годом!"
+  },
+  "cousin": { //?name=cousin
+    "name": "Дима, Даша и Свят",
+    "text1": "Привет, «родичи»)",
+    "text2": "Ярких эмоций и счастья без меры.",
+    "text3": "С Новым Годом!"
+  },
+  "godmother": { //?name=godmother
+    "name": "Руслан и Юля",
+    "text1": "Мои самые близкие и родные.",
+    "text2": "Спасибо за то, что Вы у меня есть)",
+    "text3": "С Новым Годом!"
+  },
+  "goddaughter": { //?name=goddaughter
+    "name": "Дашенька",
+    "text1": "Моя любимая крестница!",
+    "text2": "Будь такой же яркой и красивой.",
+    "text3": "С Новым Годом!"
+  },
+  "godfather": { //?name=godfather
+    "name": "Эдик и Ира",
+    "text1": "Дорогие мои кумовья)",
+    "text2": "Семейного счастья и любви до «безтями»",
+    "text3": "С Новым Годом!"
+  }, 
+  "friends": { //?name=friends
+    "name": "Саша, Катя и Женя",
+    "text1": "Самая классная семья)",
+    "text2": "Здоровья, счастья и радости в доме.",
+    "text3": "С Новым Годом!"
+  },
+  "dasha": { //?name=dasha
+    "name": "Даша",
+    "text1": "«Воронько» ты моя «Воронько»",
+    "text2": "Пусть глаза твои счастьем сияют всегда)",
+    "text3": "С Новым Годом!"
+  },
+  "vova": { //?name=vova
+    "name": "Вова",
+    "text1": "Надежный друг и шкипер)",
+    "text2": "Попутного ветра и семь футов под килем, по жизни",
+    "text3": "С Новым Годом!"
+  },
+  "natali": { //?name=natali
+    "name": "Натали",
+    "text1": "Конечно, для тебя это не будет сюрпризом.",
+    "text2": "Но очень хочу, что бы ты улыбнулась)",
+    "text3": "С Новым Годом!"
+  }
+};
+const myurl_string = window.location.href;
+const myurl = new URL(myurl_string);
+const nameurl = myurl.searchParams.get('name');
+const names = document.getElementById('names');
+const firstRow = document.getElementById('row_1');
+const secondRow = document.getElementById('row_2');
+const thirtRow = document.getElementById('row_3');
+let arr = [];
+let newArr = [];
+
+function addTags() {
+  arr.forEach(function(el) {
+    newArr.push('<span>');
+    newArr.push(el);
+    newArr.push('</span>');  
+  });
+}
+function addText(par) {
+  names.innerText = par.name;
+  arr = par.text1.split(" ");
+  newArr = [];
+  addTags();
+  firstRow.innerHTML = newArr.join('');
+  arr = par.text2.split(" ");
+  newArr = [];
+  addTags();
+  secondRow.innerHTML = newArr.join('');
+  arr = par.text3.split(" ");
+  newArr = [];
+  addTags();
+  thirtRow.innerHTML = newArr.join('');
+}
+
+switch (nameurl) {
+  case 'parents':
+    addText(congratulations.parents);
+    break;
+  case 'brother':
+    addText(congratulations.brother);
+    break;
+  case 'nephews':
+    addText(congratulations.nephews);
+    break;
+  case 'cousin':
+    addText(congratulations.cousin);
+    break;
+  case 'godmother':
+    addText(congratulations.godmother);
+    break;
+    case 'goddaughter':
+    addText(congratulations.goddaughter);
+    break;
+  case 'godfather':
+    addText(congratulations.godfather);
+    break;
+  case 'friends':
+    addText(congratulations.friends);
+    break;
+  case 'dasha':
+    addText(congratulations.dasha);
+    break;
+  case 'vova':
+    addText(congratulations.vova);
+    break;
+  case 'natali':
+    addText(congratulations.natali);
+    break;
+}
